20babecc16a9ff4637fa04900379e11c717ab11f
it24100163 <it24100163@my.sliit.lk>
2026-05-05 01:36:45 +0530

    Update reviewRoutes.js

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); 
 const router = express.Router();
 const ctrl = require('../controllers/reviewController');
 const { protect, requireRole } = require('../middleware/authMiddleware');
