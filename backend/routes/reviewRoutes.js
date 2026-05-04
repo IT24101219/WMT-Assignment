2f30d4c71c14cf71b6c0b97c8cab10c66630e8ab
it24100163 <it24100163@my.sliit.lk>
2026-05-05 01:37:35 +0530

    Fix formatting of updateReview route in reviewRoutes

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -7,7 +7,7 @@ router.get('/', protect, ctrl.getReviews);
 router.get('/stats/:movieId', protect, ctrl.getMovieStats);
 router.get('/:id', protect, ctrl.getReviewById); 
 router.post('/', protect, requireRole('customer'), ctrl.createReview);
-router.put('/:id', protect, requireRole('customer'), ctrl.updateReview);
+router.put('/:id', protect, requireRole('customer'), ctrl.updateReview); 
 router.put('/:id/moderate', protect, requireRole('branch_manager', 'main_manager'), ctrl.moderateReview);
 router.delete('/:id', protect, ctrl.deleteReview);
 
